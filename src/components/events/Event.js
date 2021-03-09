6e1a78b97dcf08664b10a634133596e3e483c218
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-09 15:33:52 -0600

    factored EventForm.js Next step is to debug

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-//! Double check class names with wireframe
+
 // Renders event card minimally with only a title as clickable link to send to details page
 export const EventCard = ({event}) => (
-    <section className="event__item">
-        <h3 className="event__title">
+    <section className="eventCard">
+        <h3 className="eventTitle">
             <Link to ={`events/detail/${evemt.id}`}>
                 {event.name}
             </Link>
--- src/components/events/EventForm.js
+++ src/components/events/EventForm.js
@@ -2,7 +2,108 @@ import React, { useContext, useEffect, useState } from "react"
 import { EventContext } from "./EventProvider"
 import { useParams, useHistory } from "react-router-dom"
 
-//! Fill out form for adding events
+
+
 export const EventForm = () => {
 
-}
+    const { getEvents, addEvent, updateEvent, getEventById } = useContext(EventContext)
+
+    const [event, setEvent] = useState({
+        date: "",
+        userId: 0,
+        title: "",
+        info: "",
+    })
+    
+    const [isLoading, setIsLoading] = useState(true)
+    const { eventId } = useParams()
+
+    const history = useHistory()
+
+    useEffect(() => {
+        getEvents()
+            .then(() => {
+                if (eventId) {
+                    getEventById(eventId)
+                        .then(event => {
+                            setEvent(event)
+                            setIsLoading(false)
+                        })
+                } else {
+                    setIsLoading{false}
+                }
+            })
+    }, [])
+
+    
+    const handleControlledInputChange = event => {
+
+        const newEvent = {...event}
+        // "+" is the unary operator for "parseInt()" [line 45]
+        let selectedVal = event.target.value
+        if (event.target.id.includes("Id")) {
+            selectedVal = +selectedVal
+        }
+
+        newEvent[event.target.id] = selectedVal
+
+        setEvent(newEvent)
+    }
+    
+    const handleSaveEvent = () => {
+
+        
+        setIsLoading(true)
+        const currentUserId = sessionStorage.getItem("id")
+        if(eventId) {
+            updateEvent({
+                date: event.date,
+                userId: currentUserId,
+                title: event.title,
+                info: event.info,
+                id: event.id
+            }).then(() => history.push(`/events/detail/${event.id}`))
+        } else {
+            addEvent({
+                date: event.date,
+                userId: currentUserId,
+                title: event.title,
+                info: event.info,
+            }).then(() => history.push("/events"))
+        }
+    }
+
+    return (
+        <form className="eventForm">
+            <h2 className="eventFormTitle">{eventId ? "Edit Event" : "Add Event"}</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="title">Event Title: </label> 
+                    <input type="title" id="title" onChange={handleControlledInputChange} required autofocus className="form-control" placeholder="title?" value={event.title} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="date">Event Date: </label> 
+                    <input type="date" id="date" onChange={handleControlledInputChange} required className="form-control" placeholder="date?" value={event.date} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="info">Event Description: </label> 
+                    <input type="info" id="info" onChange={handleControlledInputChange} required className="form-control" placeholder="info?" value={event.info} />
+                </div>
+            </fieldset>
+            <button className="btn btn-primary"
+                disabled={isLoading}
+                onClick={event => {
+                    event.preventDefault()
+                    handleSaveEvent()
+                }}>
+                    {eventId ? "Save Event" : "Add Event"}
+                </button>
+        </form>
+    )
+
+    
+}   

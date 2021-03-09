d06a69b5ce6e0a9f936ecd1ea4aa679245b592af
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-09 15:06:52 -0600

    factored EventList.js

---
+++ src/components/events/EventList.js
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect } from "react"
+import { EventContext } from "./EventProvider"
+import { EventCard } from "./Event"
+import { useHistory } from "react-router"
+
+export const EventList = () => {
+
+    const { events, getEvents } = useContext(EventContext)
+
+    useEffect(() => {
+        getEvents()
+    }, [])
+
+    const history = useHistory()
+
+    return (
+        <>
+            <h2>Events</h2>
+                <button onClick={() => {history.push("/events/create")}}>
+                    Create Event
+                </button>
+            <div className="eventList">
+                {
+                    events.map(event => {
+                        return <EventCard key={event.id} event={event} />
+                    })
+                }
+            </div>
+        </>
+    )
+}

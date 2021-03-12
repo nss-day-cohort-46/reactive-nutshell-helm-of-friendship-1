7b60b42be05278da96656d9e1435366775b30ef8
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-12 13:07:02 -0600

    attempting to fix merge

--- src/components/friends/FriendList.js
+++ src/components/friends/FriendList.js
@@ -4,6 +4,8 @@ import { UserContext } from "../users/UserProvider"
 import { FriendCard } from "./Friend"
 import { FriendContext } from "./FriendProvider"
 
+//! Working on a merge fix
+
 export const FriendList = () => {
     const { friends, getFriends } = useContext(FriendContext)
     const { users, getUsers, searchTerms } = useContext(UserContext)

164dc0970662515fb9c8adb62981dd26e6bfdfa5
Loganwebb1994 <loganwebb1994@gmail.com>
2021-03-12 09:59:25 -0600

    user now sees friend's articles and user articles

--- src/components/articles/ArticleList.js
+++ src/components/articles/ArticleList.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from "react"
+import React, {useContext, useEffect, useState} from "react"
 import {useHistory} from "react-router-dom"
 import {ArticleContext} from "./ArticleProvider"
 import {FriendContext} from "../friends/FriendProvider"
@@ -9,7 +9,34 @@ export const ArticleList = () =>{
 
 const history = useHistory()
 
-const {articles, getArticles, setArticles} = useContext(ArticleContext)
+const {articles, getArticles} = useContext(ArticleContext)
+const {getUsersFriends, friends} = useContext(FriendContext)
+const [userArticles, setUserArticles] = useState([])
+const currentUserId = +sessionStorage.getItem("nutshell_user")
+
+useEffect(() =>{
+  getUsersFriends(currentUserId)
+  .then(getArticles)
+}, [])
+
+useEffect (() =>{
+  
+  const matchingCurrentUserArticles = articles.filter(article => (currentUserId === article.userId))
+  
+  const matchingFriendArticles = articles.filter(article =>{
+    return friends.map(friend => friend.userId === article.userId)
+  })
+  let filteredMatchingFriendArticles = []
+  matchingFriendArticles.forEach(obj =>{
+    if(obj.userId !== currentUserId){
+      filteredMatchingFriendArticles.push(obj)
+    }
+  })
+
+  const allArticleMatches = matchingCurrentUserArticles.concat(filteredMatchingFriendArticles)
+  setUserArticles(allArticleMatches)
+}, [articles, friends])
+
 
 
 
